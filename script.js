5069026d8c6b00b3e014f5b83b47d77950a8a47c
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-15 06:34:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let inputs=document.querySelectorAll(".code");
 inputs[0].focus();
 for(let i=0;i<inputs.length;i++){
 	inputs[i].addEventListener("input",()=>{
-		if(inputs[i].value!=""){
+		if(inputs[i].value!="" && i<inputs.length-1 ){
 			inputs[i+1].focus();
 		}
 	});
 	inputs[i].addEventListener("keydown",(e)=>{
-		if(e.key==="Backspace"){
+		if(e.key==="Backspace" && i>0){
 			inputs[i].value="";
 			inputs[i-1].focus()
 			
